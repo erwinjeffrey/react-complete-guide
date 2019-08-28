8d4062b5199a02ce8782bd341616897fae32ac43
erwin <erwinjefly@gmail.com>
2019-08-28 15:54:17 -0400

    jrx is not html

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <h1>Hi, I'm a React App</h1>
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
       </div>
     );
+    // this code below get compiled to this code
+    /*return React.createElement(
+      'div',
+      null,
+      React.createElement('h1', { className: 'App' }, "Hi, I'm a React App!!!")
+    );*/
   }
 }
 
