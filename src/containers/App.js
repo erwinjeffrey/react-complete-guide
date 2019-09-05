a309fc351a762404223c7813f8d055005ce7458d
System Administrator <root@Maxs-MacBook-Pro.local>
2019-09-05 16:52:50 -0400

    Component Update Lifecycle(for state changes)

--- src/containers/App.js
+++ src/containers/App.js
@@ -36,6 +36,16 @@ class App extends Component {
   componentDidMount() {
     console.log('[App.js] componentDidMount');
   }
+
+  /* Component lifecycle for state(for state changes) */
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[App.js] shouldComponentUpdate');
+    return true;
+  }
+
+  componentDidUpdate() {
+    console.log('[App.js] componentDidUpdate');
+  }
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
       return p.id === id;
