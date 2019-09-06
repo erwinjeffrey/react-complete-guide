61348d8d27ca0bfe39e2e85d36c81a2172fcf899
System Administrator <root@Maxs-MacBook-Pro.local>
2019-09-06 16:32:30 -0400

    optimizing Functional Components with React.memo()

--- src/components/Cockpit/CockPit.js
+++ src/components/Cockpit/CockPit.js
@@ -3,21 +3,16 @@ import React, { useEffect } from 'react';
 import classes from './CockPit.css';
 
 const cockpit = props => {
-  // it execute for every render cycle, it is a react hook for funcional componet
-  // combination of componentDidMount and componetDidUpdate
-  //it's for the important lifecycle hook(those ones we use in the base class component)
   useEffect(() => {
     console.log('[Cockpit.js] useEffect');
     // you make http request here...
     const timer = setTimeout(() => {
       alert('Saved data to cloud!');
-    }, 1000); // if we pass it [] an empty array, use effect will run once, for the first time and then never, this is equl to componentDidMount
-    // will run before the component it destroy, it equivalents to ComponentWillUnmount in base class component
+    }, 1000);
     return () => {
-      clearTimeout(timer);
       console.log('[Cockpit.js] cleanup work in useEffect');
     };
-  }, [props.persons]); //by using [props.persons],we're telling useeffect to run everytime persons change
+  }, [props.persons]);
 
   // this run  will in every update cycle with no arguments
   useEffect(() => {
@@ -26,16 +21,17 @@ const cockpit = props => {
       console.log('[Cockpit.js] cleanup work in 2nd useEffect');
     };
   });
+
   const assignedClasses = [];
   let btnClass = '';
   if (props.showPersons) {
     btnClass = classes.Red;
   }
 
-  if (props.persons.length <= 2) {
+  if (props.personsLength <= 2) {
     assignedClasses.push(classes.red); // classes = ['red']
   }
-  if (props.persons.length <= 1) {
+  if (props.personsLength <= 1) {
     assignedClasses.push(classes.bold); // classes = ['red','bold']
   }
 
@@ -50,5 +46,6 @@ const cockpit = props => {
     </div>
   );
 };
-
-export default cockpit;
+//React.memo: this is memoization to store a snapshot of this component
+//and if it input changes it will render it.
+export default React.memo(cockpit);
