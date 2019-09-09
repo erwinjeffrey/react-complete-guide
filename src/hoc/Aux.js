f70d41417f127d2495081a4241adac8766dbde61
System Administrator <root@Maxs-MacBook-Pro.local>
2019-09-09 16:19:39 -0400

    Another Form of High order component

--- src/hoc/Aux.js
+++ src/hoc/Aux.js
@@ -1,5 +1,2 @@
 const aux = props => props.children;
-/*use Aux when you don't want a wrapping component in your DOM, 
-//it can be because of some styling issue
-just a tecnical wrapper*/
 export default aux;
